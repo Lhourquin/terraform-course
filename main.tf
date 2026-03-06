19605fb6041a2ef785d52b94249f9dbf8ed68c5b
lhourquin <lhourquin@gmail.com>
2026-03-06 14:43:44 +0100

    Initialize Terraform configuration with local and null providers, create a local file resource, and add initial state files.

---
+++ main.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+provider "local" {}
+resource "local_file" "test_file" {
+  content  = "Hello Lucas !"
+  filename = "${path.module}/hello.txt"
+}
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Commande exécutée !'"
+  }
+}
